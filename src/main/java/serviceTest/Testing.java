8f7f888d8933f4b15a4030aedabbb4690dc136d9
phuc98ute <vophuc3@gmail.com>
2021-08-27 21:06:10 +0700

    create commit

---
+++ src/main/java/serviceTest/Testing.java
@@ -0,0 +1,14 @@
+package serviceTest;
+
+/**
+ * Created by : phucvo
+ * Date       : 8/27/21
+ * Time       : 7:54 PM
+ * Package    : serviceTest
+ * Filename   : Testing
+ */
+public class Testing {
+    public void display(String param) {
+        System.out.println("YOur text as artifact " +param);
+    }
+}
